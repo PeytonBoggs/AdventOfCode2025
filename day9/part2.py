cab89349fa7a35d5829c570cc5fdea24537bd0f5
Peyton <peytonrva@gmail.com>
2025-12-10 02:07:59 -0500

    day 9

--- day9/part2.py
+++ day9/part2.py
@@ -1,3 +1,6 @@
+import math
+from scipy.sparse import dok_array
+
 file = open("day9/input.txt", "r").readlines()
 
 red_tiles = []
@@ -6,74 +9,108 @@ for line in file:
     line[0], line[1] = int(line[1]), int(line[0])
     red_tiles.append(line)
 
-# Add first tile to the end so that the list wraps around
-red_tiles.append(red_tiles[0])
-
-max_row = max(red_tiles, key=lambda tile: tile[0])[0]
-max_col = max(red_tiles, key=lambda tile: tile[1])[1]
+max_row = max(red_tiles, key=lambda tile: tile[0])[0] + 1
+max_col = max(red_tiles, key=lambda tile: tile[1])[1] + 1
 
 min_row = min(red_tiles, key=lambda tile: tile[0])[0]
 min_col = min(red_tiles, key=lambda tile: tile[1])[1]
 
-print("Max row:", max_row, "Min row:", min_row, "Max col:", max_col, "Min col:", min_col)
+# Create sparse 2d array that is the size of the graph to represent the border tiles, initialize with all False
+border_tile_matrix = dok_array((max_row, max_col), dtype=bool)
 
-boundary_tiles = []
+# Add corner (red) tiles to border tile matric
+for tile in red_tiles:
+    border_tile_matrix[tile[0], tile[1]] = True
 
-# Add boundary tiles
+# Add first tile to the end so that the list wraps around
+red_tiles.append(red_tiles[0])
+
+# Add border tiles
 for tile in range(len(red_tiles) - 1):
     # If the next tile is left or right
     if red_tiles[tile][0] == red_tiles[tile+1][0]:
         # If going left
         if red_tiles[tile][1] > red_tiles[tile+1][1]:
             for col in range(red_tiles[tile][1], red_tiles[tile+1][1], -1):
-                boundary_tiles.append([red_tiles[tile][0], col])
+                border_tile_matrix[red_tiles[tile][0], col] = True
         # If going right
         if red_tiles[tile][1] < red_tiles[tile+1][1]:
             for col in range(red_tiles[tile][1], red_tiles[tile+1][1]):
-                boundary_tiles.append([red_tiles[tile][0], col])
+                border_tile_matrix[red_tiles[tile][0], col] = True
 
     # If the next tile is up or down, put X's in between
     if red_tiles[tile][1] == red_tiles[tile+1][1]:
         # If going up
         if red_tiles[tile][0] > red_tiles[tile+1][0]:
             for row in range(red_tiles[tile][0], red_tiles[tile+1][0], -1):
-                boundary_tiles.append([row, red_tiles[tile][1]])
+                border_tile_matrix[row, red_tiles[tile][1]] = True
         # If going right
         if red_tiles[tile][0] < red_tiles[tile+1][0]:
             for row in range(red_tiles[tile][0], red_tiles[tile+1][0]):
-                boundary_tiles.append([row, red_tiles[tile][1]])
+                border_tile_matrix[row, red_tiles[tile][1]] = True
 
-print(len(boundary_tiles))
+# Create sparse 2d array that is the size of the graph to represent the border tiles, initialize with all False
+inside_tile_matrix = dok_array((max_row, max_col), dtype=bool)
 
-# Check if a tile is inside the boundary
-def is_inside(tile):
-    if tile in boundary_tiles:
+# Definitively determine if a tile is inside the border by counting border tiles in all directions
+def determine_tile_inside(row, col):
+    # If on the border, tile is not inside
+    if border_tile_matrix[row, col]:
+        return False
+
+    # Count border tiles to the left
+    count_left = 0
+    for i in range(0, col):
+        if border_tile_matrix[row, i]:
+            count_left += 1
+
+    # Count border tiles to the right
+    count_right = 0
+    for i in range(col + 1, max_col):
+        if border_tile_matrix[row, i]:
+            count_right += 1
+
+    # Count border tiles up
+    count_up = 0
+    for i in range(0, row):
+        if border_tile_matrix[i, col]:
+            count_up += 1
+
+    # Count border tiles down
+    count_down = 0
+    for i in range(row + 1, max_row):
+        if border_tile_matrix[i, col]:
+            count_down += 1
+
+    # Axiom: if there is an odd number of border crossings in all directions, a tile is in the boundary
+    if count_left % 2 == 1 and count_right % 2 == 1 and count_up % 2 == 1 and count_down % 2 == 1:
         return True
-    
-    boundary_tiles_in_row = list(filter(lambda boundary_tile: boundary_tile[0] == tile[0], boundary_tiles))
-    boundary_tiles_in_col = list(filter(lambda boundary_tile: boundary_tile[1] == tile[1], boundary_tiles))
 
-    min_boundary_row = min(boundary_tiles_in_col, key=lambda tile: tile[0])[0]
-    max_boundary_row = max(boundary_tiles_in_col, key=lambda tile: tile[0])[0]
+# Fill the inside of the matrix by recursively checking around inside tiles after initializing one
+def fill_tile_inside():
+    inside_stack = []
+    directions = [(1, 0), (1, -1), (1, 1), (0, 1), (0, -1), (-1, 1), (-1, 0), (-1, -1)]
+    for direction in directions:
+        # When the inital inside tile is found
+        if determine_tile_inside(red_tiles[0][0] + direction[0], red_tiles[0][1] + direction[1]):
+            # Add to the sparse matrix
+            inside_tile_matrix[red_tiles[0][0] + direction[0], red_tiles[0][1] + direction[1]] = True
+            # Add to the stack
+            inside_stack.append([red_tiles[0][0] + direction[0], red_tiles[0][1] + direction[1]])
+            break
 
-    min_boundary_col = min(boundary_tiles_in_row, key=lambda tile: tile[1])[1]
-    max_boundary_col = max(boundary_tiles_in_row, key=lambda tile: tile[1])[1]
+    while inside_stack != []:
+        tile = inside_stack.pop()
+        for direction in directions:
+            if not inside_tile_matrix[tile[0] + direction[0], tile[1] + direction[1]] and not border_tile_matrix[tile[0] + direction[0], tile[1] + direction[1]]:
+                # Add to the stack
+                inside_stack.append([tile[0] + direction[0], tile[1] + direction[1]])
+                # Add to the sparse matrix
+                inside_tile_matrix[tile[0] + direction[0], tile[1] + direction[1]] = True
 
-    # Must have a boundary tile above, below, left, and right of the tile
+# fill_tile_inside()
 
-    if tile[0] >= min_boundary_row and tile[0] <= max_boundary_row and tile[1] >= min_boundary_col and tile[1] <= max_boundary_col:
-        return True
-
-    return False
-
-# Conditionally add the area between two tiles
-areas = []
-def add_area(tile1, tile2):
-    area = ((abs(tile1[0] - tile2[0]) + 1) * (abs(tile1[1] - tile2[1]) + 1))
-    areas.append(area)
-
-# Returns whether or not all tiles between two tiles are valid
-def valid_area(tile1, tile2):
+def check_valid_rug(tile1, tile2):
     # If any of the tiles in the rug are outside the boundary, the rug will be invalid
     # Find the rug's borders
     if tile1[0] == tile2[0]:
@@ -90,30 +127,35 @@ def valid_area(tile1, tile2):
     else:
         startj, endj = tile2[1] + 1, tile1[1]
 
-    # print("Validating area", tile1, tile2)
+    # Include step to speed up
+    stepi = math.ceil((endi - starti) / 50)
+    stepj = math.ceil((endj - startj) / 50)
 
     # Look through each of the rug's tiles
-    for i in range(starti, endi):
-        for j in range(startj, endj):
-            # print("Checking tile", i, j)
-            if not is_inside([i, j]):
+    for i in range(starti, endi, stepi):
+        for j in range(startj, endj, stepj):
+            # if not determine_tile_inside(i, j):
+            #     return False
+            # if not border_tile_matrix[i, j] and not inside_tile_matrix[i, j]:
+            #    return False
+            if border_tile_matrix[i, j]:
                 return False
+
+    # Check opposite corners
+    if not border_tile_matrix[tile1[0], tile2[1]] and not border_tile_matrix[tile2[0], tile1[1]]:
+        return False
 
     return True
 
+max_area = 0
+max_pair = []
 num_counted = 0
-areas = []
-for tile in red_tiles:
-    for other_tile in red_tiles:
-        areas.append([(abs(tile[0] - other_tile[0]) + 1) * (abs(tile[1] - other_tile[1]) + 1), tile, other_tile])
+for tile1 in red_tiles:
+    print(num_counted)
+    for tile2 in red_tiles:
+        area = (abs(tile1[0] - tile2[0]) + 1) * (abs(tile1[1] - tile2[1]) + 1)
+        if area > max_area and check_valid_rug(tile1, tile2):
+            max_area = area
     num_counted += 1
 
-areas.sort(reverse=True)
-
-print(areas[0])
-
-# for area in areas:
-#     print("Checking area", area)
-#     if valid_area(area[1], area[2]):
-#         print("Largest valid area", area[0])
-#         break
+print(max_area)
