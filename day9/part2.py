b97dcd2eeeefa081f0cb142584bc35f0c93af2b3
Peyton <peytonrva@gmail.com>
2025-12-12 18:53:56 -0500

    day 9

--- day9/part2.py
+++ day9/part2.py
@@ -1,15 +1,27 @@
-import math
-from functools import lru_cache
-from scipy.sparse import dok_array, coo_array
+import functools
+from scipy.sparse import dok_array
+import multiprocessing as mp
 
 file = open("day9/input.txt", "r").readlines()
 
-red_tiles = []
+expanded_red_tiles = []
 for line in file:
     line = line.strip().split(",")
-    line[0], line[1] = int(line[1]), int(line[0])
-    red_tiles.append(line)
+    # Reverse the rows and cols to be consistent with question
+    expanded_red_tiles.append([int(line[1]), int(line[0])])
 
+# Compress the coordinates
+expanded_rows = [tile[0] for tile in expanded_red_tiles]
+expanded_rows = sorted(list(set(expanded_rows)))
+row_dict = {row: i for i, row in enumerate(expanded_rows)}
+
+expanded_cols = [tile[1] for tile in expanded_red_tiles]
+expanded_cols = sorted(list(set(expanded_cols)))
+col_dict = {col: j for j, col in enumerate(expanded_cols)}
+
+red_tiles = [[row_dict[tile[0]], col_dict[tile[1]]] for tile in expanded_red_tiles]
+
+# Find the min/max coordinates
 max_row = max(red_tiles, key=lambda tile: tile[0])[0] + 1
 max_col = max(red_tiles, key=lambda tile: tile[1])[1] + 1
 
@@ -19,12 +31,31 @@ min_col = min(red_tiles, key=lambda tile: tile[1])[1]
 # Create sparse 2d array that is the size of the graph to represent the border tiles, initialize with all False
 border_tile_matrix = dok_array((max_row, max_col), dtype=bool)
 
-# Add corner (red) tiles to border tile matric
+# Add corner (red) tiles to border tile matrix
 for tile in red_tiles:
     border_tile_matrix[tile[0], tile[1]] = True
 
 # Add first tile to the end so that the list wraps around
 red_tiles.append(red_tiles[0])
+
+# Print tiles before adding borders
+'''
+print("   ", end="")
+
+for j in range(max_col):
+    print(str(j % 100).zfill(2), end="")
+
+print()
+
+for i in range(max_row):
+    print(str(i).zfill(3), end="")
+    for j in range(max_col):
+        if border_tile_matrix[i, j] == True:
+            print("# ", end="")
+        else:
+            print(". ", end="")
+    print()
+'''
 
 # Add border tiles
 for tile in range(len(red_tiles) - 1):
@@ -50,13 +81,36 @@ for tile in range(len(red_tiles) - 1):
             for row in range(red_tiles[tile][0], red_tiles[tile+1][0]):
                 border_tile_matrix[row, red_tiles[tile][1]] = True
 
-# Definitively determine if a tile is inside the border by counting border tiles in all directions
-@lru_cache
-def determine_tile_inside(row, col):
-    # If on the border, tile is not inside
+print("Border tiles added")
+# Print tiles after adding borders
+'''
+print("   ", end="")
+
+for j in range(max_col):
+    print(str(j % 100).zfill(2), end="")
+
+print()
+
+for i in range(max_row):
+    print(str(i).zfill(3), end="")
+    for j in range(max_col):
+        if border_tile_matrix[i, j] == True:
+            print("# ", end="")
+        else:
+            print(". ", end="")
+    print()
+'''
+
+# Determines if a tile is inside or on the border
+@functools.cache
+def determine_valid_tile(row, col):
+    # If on the border, tile is valid
     if border_tile_matrix[row, col]:
-        return False
-    
+        return True
+
+    # If a tile has a boundary tile above, below, left, and right, it is valid (this method has edge cases, but those do not appear in the input)
+
+    # Get all tiles in the tile's row (vertical intersection cols) and col (horizontal intersection rows)
     horizontal_intersection_cols = []
     for key in border_tile_matrix[[row]].keys():
         horizontal_intersection_cols.append(key[1])
@@ -65,73 +119,111 @@ def determine_tile_inside(row, col):
     for key in border_tile_matrix[:, [col]].keys():
         vertical_intersection_rows.append(key[0])
 
-    # Edge case: remove rows/cols that form a straight line
-    j = 0
-    while j < len(horizontal_intersection_cols) - 1:
-        if horizontal_intersection_cols[j] == horizontal_intersection_cols[j+1] - 1:
-            horizontal_intersection_cols.remove(horizontal_intersection_cols[j])
-            j -= 1
-        j += 1
+    min_boundary_row = min(vertical_intersection_rows)
+    max_boundary_row = max(vertical_intersection_rows)
 
-    i = 0
-    while i < len(vertical_intersection_rows) - 1:
-        if vertical_intersection_rows[i] == vertical_intersection_rows[i+1] - 1:
-            vertical_intersection_rows.remove(vertical_intersection_rows[i])
-            i -= 1
-        i += 1
-    
-    count_left = len(list(filter(lambda j: j < col, horizontal_intersection_cols)))
-    count_right = len(horizontal_intersection_cols) - count_left
+    min_boundary_col = min(horizontal_intersection_cols)
+    max_boundary_col = max(horizontal_intersection_cols)
 
-    count_up = len(list(filter(lambda i: i < row, vertical_intersection_rows)))
-    count_down = len(vertical_intersection_rows) - count_up
-
-    # Axiom: if there is an odd number of border crossings in all directions, a tile is in the boundary
-    if count_left % 2 == 1 and count_right % 2 == 1 and count_up % 2 == 1 and count_down % 2 == 1:
+    if row >= min_boundary_row and row <= max_boundary_row and col >= min_boundary_col and col <= max_boundary_col:
         return True
+    
     return False
 
-def check_valid_rug(tile1, tile2):
-    # If any of the tiles in the rug are outside the boundary, the rug will be invalid
-    # Find the rug's borders
-    if tile1[0] == tile2[0]:
-        starti, endi = tile1[0], tile1[0] + 1
-    elif tile1[0] < tile2[0]:
-        starti, endi = tile1[0], tile2[0] + 1
+ # Checks a rug for validity; if any of the tiles of the rug's boundary are not a red or greed (valid) tile, the rug will be invalid
+def check_valid_rug(corners):
+    # Find the rug's boundary indices
+    if corners[0][0] == corners[1][0]:
+        start_row, end_row = corners[0][0], corners[0][0]
+    elif corners[0][0] < corners[1][0]:
+        start_row, end_row = corners[0][0], corners[1][0]
     else:
-        starti, endi = tile2[0], tile1[0] + 1
+        start_row, end_row = corners[1][0], corners[0][0]
 
-    if tile1[1] == tile2[1]:
-        startj, endj = tile1[1], tile1[1] + 1
-    elif tile1[1] < tile2[1]:
-        startj, endj = tile1[1], tile2[1] + 1
+    if corners[0][1] == corners[1][1]:
+        start_col, end_col = corners[0][1], corners[0][1]
+    elif corners[0][1] < corners[1][1]:
+        start_col, end_col = corners[0][1], corners[1][1]
     else:
-        startj, endj = tile2[1], tile1[1] + 1
-
-    # Include step to speed up
-    stepi = math.ceil((endi - starti) / 2)
-    stepj = math.ceil((endj - startj) / 2)
-
-    # Look through each of the rug's tiles
-    for i in range(starti, endi, stepi):
-        for j in range(startj, endj, stepj):
-            if not border_tile_matrix[i, j] and not determine_tile_inside(i, j):
+        start_col, end_col = corners[1][1], corners[0][1]
+    
+    # Iterate through all tiles
+    for i in range(start_row, end_row + 1):
+        for j in range(start_col, end_col + 1):
+            if not determine_valid_tile(i, j):
                 return False
-
+        
     return True
 
-max_area = 0
-max_pair = []
-num_counted = 0
-num_inner_counted = 0
-for tile1 in red_tiles:
-    for tile2 in red_tiles:
-        area = (abs(tile1[0] - tile2[0]) + 1) * (abs(tile1[1] - tile2[1]) + 1)
-        if area > max_area and check_valid_rug(tile1, tile2):
-            max_area = area
-        print(num_inner_counted)
-        num_inner_counted += 1
-    print(num_counted)
-    num_counted += 1
+# Returns the area of a rug
+def rug_area(tile1, tile2):
+    return (abs(tile1[0] - tile2[0]) + 1) * (abs(tile1[1] - tile2[1]) + 1)
 
-print(max_area)
+# Undoes the coordinate compression to get the original coordinates for a tile
+def expand_tile_coordinates(tile):
+    return_row = -1
+    return_col = -1
+    
+    for expanded_row, row in row_dict.items():
+        if row == tile[0]:
+            return_row = expanded_row
+
+    for expanded_col, col in col_dict.items():
+        if col == tile[1]:
+            return_col = expanded_col
+    
+    # print(return_row, return_col)
+    return [return_row, return_col]
+
+# In order to do multiprocessing, we first have to find a list of max_area candidates, then run to determine if valid
+def get_areas():
+    areas=[]
+    num_counted = 0
+    for i in range(len(red_tiles)):
+        # print("OUTER LOOP:", num_counted)
+        #num_inner = 0
+        for j in range(i, len(red_tiles)):
+            #print(num_inner)
+            areas.append([rug_area(expand_tile_coordinates(red_tiles[i]), expand_tile_coordinates(red_tiles[j])), i, j])
+            #num_inner += 1
+        num_counted += 1
+    
+    areas.sort(reverse=True)
+    return(areas)
+
+# Find areas of all potential rugs (have to do this first in order to multiprocess over all of them later)
+print("Getting areas")
+areas = get_areas()
+print("Areas returned")
+
+# Search through set of areas until first valid area is found
+max_area_found = False
+search_start = 0
+search_end = 20
+while not max_area_found:
+    print("Searching areas from", search_start, "to", search_end)
+    search_areas = areas[search_start:search_end]
+
+    # Put the pairs to validate into a list that is comprehensible for multithreading
+    potential_max_pairs = []
+    for area in search_areas:
+        potential_max_pairs.append(tuple([red_tiles[area[1]], red_tiles[area[2]]]))
+
+    # Multithread the check_valid_rug function
+    with mp.Pool() as pool:
+        valid_areas = (pool.map(check_valid_rug, potential_max_pairs))
+
+    # Take the first valid rug in the sorted list of areas
+    for i in range(len(valid_areas)):
+        if valid_areas[i] == True:
+            max_area_found = True
+            max_tile1_index = search_areas[i][1]
+            max_tile2_index = search_areas[i][2]
+            print(expand_tile_coordinates(red_tiles[max_tile1_index]), expand_tile_coordinates(red_tiles[max_tile2_index]))
+            break
+    
+    # If no rug is valid, expand the search area
+    search_start = search_end
+    search_end *= 2
+
+print(rug_area(expand_tile_coordinates(red_tiles[max_tile1_index]), expand_tile_coordinates(red_tiles[max_tile2_index])))
